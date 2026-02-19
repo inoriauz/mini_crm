9b339e025c95708f641402083736d1f11729047f
Bekzod <b.nazirbekov@inoria.uz>
2026-02-19 16:38:45 +0500

    Admin Panel: Client Resource with CRUD

---
+++ app/Filament/Resources/Clients/Schemas/ClientForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\Clients\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class ClientForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('full_name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                TextInput::make('phone')
+                    ->tel(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Clients/Schemas/ClientInfolist.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\Clients\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class ClientInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('full_name'),
+                TextEntry::make('email')
+                    ->label('Email address'),
+                TextEntry::make('phone')
+                    ->placeholder('-'),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+}
