514197894259cc6f26eca81be827c4fd1506d4df
Bekzod <b.nazirbekov@inoria.uz>
2026-02-20 10:42:02 +0500

    add ClientFactory, ClientSeeder, and ClientPolicy for admin role permissions

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'full_name',
         'email',
         'phone'
     ];
-    
+
 }
